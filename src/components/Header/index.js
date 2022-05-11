4d3c9ba31e8ed616c63bd50ffc2222468c1bafdd
alex-kovalevich <aliaksei.kavalevich@thirtymadison.com>
2022-05-11 11:36:47 +0500

    small fix

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.svg';
 import cart from '../../assets/images/shop.svg';
 import './styles.scss';
@@ -6,13 +7,13 @@ import './styles.scss';
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo">
+      <Link to="/" className="logo">
         <img className="logo_icon" src={logo} alt="logo" />
         <div className="logo_titles">
           <h1 className="logo_h1">React pizza</h1>
           <p className="logo_p">лучшая пицца во вселенной</p>
         </div>
-      </div>
+      </Link>
       <button className="header_cart">
         <img src={cart} alt="cart" />
       </button>
